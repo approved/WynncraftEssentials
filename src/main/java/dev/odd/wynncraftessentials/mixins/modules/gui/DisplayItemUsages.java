e9c940651d22ddb6c69e2a8fe23116d2d575b614
Nick Kennedy <nickkennedy334@gmail.com>
2020-05-11 17:18:53 -0400

    Created generic solution for rendering the item usage

--- src/main/java/dev/odd/wynncraftessentials/mixins/modules/gui/DisplayItemUsages.java
+++ src/main/java/dev/odd/wynncraftessentials/mixins/modules/gui/DisplayItemUsages.java
@@ -8,35 +8,40 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 
 @Mixin(ItemRenderer.class)
 public abstract class DisplayItemUsages {
 
-    @Deprecated
-    Pattern potionNameMatch = Pattern.compile("^(§[a-z0-9])?Potion of .*\\[(\\d)\\/\\d\\]");
-    
     Pattern itemUsagePattern = Pattern.compile("^.*\\[(\\d)\\/\\d\\]");
 
-    /**
-     * Displays item usages in the hotbar.
-     * TODO: Modify to target the invoked signature and modify the amountText argument and make generic
-     */
-    @Inject(at = @At("HEAD"), method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;II)V", cancellable = true)
-    public void onRenderItemOverlay(TextRenderer fontRenderer, ItemStack stack, int x, int y, CallbackInfo ci) {
-        String itemName = stack.getName().asString().trim();
-        Matcher m = itemUsagePattern.matcher(itemName);
-        if (m.matches()) {
-            this.renderGuiItemOverlay(fontRenderer, stack, x, y, m.group(1));
-            ci.cancel();
-        }
-    }
-
     @Shadow
-    public void renderGuiItemOverlay(TextRenderer fontRenderer, ItemStack stack, int x, int y, String amountText) {
+    float zOffset;
+
+    @Inject(at = @At(value = "INVOKE", target = "net/minecraft/item/ItemStack.getCount()I"),
+            method = "renderGuiItemOverlay(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V",
+            cancellable = true)
+    public void onRenderGuiItemOverlay(TextRenderer fontRenderer, ItemStack stack, int x, int y, String amountText, CallbackInfo ci) {
+        MatrixStack matrixStack = new MatrixStack();
+        if (stack.getCount() == 1 && amountText == null) {
+            String itemName = stack.getName().asString().trim();
+            Matcher m = itemUsagePattern.matcher(itemName);
+            if(m.matches()){
+                String string = m.group(1);
+                matrixStack.translate(0.0D, 0.0D, (double)(this.zOffset + 200.0F));
+                VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+                fontRenderer.draw(string, (float)(x + 19 - 2 - fontRenderer.getStringWidth(string)), (float)(y + 6 + 3), 16777215, true, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
+                immediate.draw();
+            }
+         }
     }
 }
